71c2396311258ab033b6ab808608d742436da4c1
User623-crypto <roel.nasufi@outlook.com>
2021-01-18 09:21:35 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Lab4V2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dekompreso_button = new System.Windows.Forms.Button();
+            this.kompresso_button = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dekompreso_button
+            // 
+            this.dekompreso_button.Location = new System.Drawing.Point(609, 91);
+            this.dekompreso_button.Name = "dekompreso_button";
+            this.dekompreso_button.Size = new System.Drawing.Size(164, 88);
+            this.dekompreso_button.TabIndex = 0;
+            this.dekompreso_button.Text = "Zgjidh Folderin qe do dekompresosh";
+            this.dekompreso_button.UseVisualStyleBackColor = true;
+            this.dekompreso_button.Click += new System.EventHandler(this.dekompreso_button_Click);
+            // 
+            // kompresso_button
+            // 
+            this.kompresso_button.Location = new System.Drawing.Point(92, 69);
+            this.kompresso_button.Name = "kompresso_button";
+            this.kompresso_button.Size = new System.Drawing.Size(203, 91);
+            this.kompresso_button.TabIndex = 1;
+            this.kompresso_button.Text = "Zgjidh folderin që do kompresosh";
+            this.kompresso_button.UseVisualStyleBackColor = true;
+            this.kompresso_button.Click += new System.EventHandler(this.kompresso_button_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(408, 246);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(102, 21);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "multThread";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.kompresso_button);
+            this.Controls.Add(this.dekompreso_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button dekompreso_button;
+        private System.Windows.Forms.Button kompresso_button;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4V2
+{
+
+    public partial class Form1 : Form
+    {
+        Stopwatch stopwatch;
+        string zipPath = "C:\\kot";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void kompresso_button_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                stopwatch = Stopwatch.StartNew();
+                Console.WriteLine(fbd.SelectedPath.ToString());
+                string path = fbd.SelectedPath.ToString();
+                Console.WriteLine("Pathi eshte: " + path);
+
+                // CompressDirectory(path, "testing.gz", (fileName) => { Console.WriteLine("Compressing {0}...", fileName); },checkBox1.Checked);
+                CompressDirectoryV2(path, checkBox1.Checked);
+                stopwatch.Stop();
+                label1.Text = "Koha eshte:" + stopwatch.ElapsedMilliseconds + "  ms";
+                Console.WriteLine("************ " + stopwatch.ElapsedMilliseconds);
+
+            }
+
+        }
+
+        public void CompressFile(string path, GZipStream zipStream)
+        {
+          
+            //Compress file content
+            byte[] bytes = File.ReadAllBytes((path)); //Merr direktorin absolute, pra të dhënat e  file-it kthehen ne byte
+            zipStream.Write(bytes, 0, bytes.Length); //Shkruan të dhënën
+        }
+        public void CompressDirectoryV2(string inDir,bool multi)
+        {
+            string[] sFiles = Directory.GetFiles(inDir, "*.*", SearchOption.AllDirectories); //Merr filet
+            foreach (string sFilePath in sFiles)
+            {
+                string sRelativePath = sFilePath.Substring(inDir.Length + 1); //File ka emrin e till C:\test\filename.txt kjo merr emrin relativ pra filename.txt
+           
+                if (multi)
+                {
+                    new Thread(() => {
+                        using (FileStream outFile = new FileStream(zipPath + "\\" + sRelativePath + ".gz", FileMode.Create, FileAccess.Write, FileShare.None)) // Krijon nje file me emrin e dhene, emri ketu eshte dhene standart testing.gz
+                        using (GZipStream str = new GZipStream(outFile, CompressionMode.Compress)) // Krijon nje Zip Stream ku do te kompresohen filet
+                            CompressFile(sFilePath, str);
+                    }).Start();
+                }
+                else
+                {
+                    using (FileStream outFile = new FileStream(zipPath + "\\" + sRelativePath + ".gz", FileMode.Create, FileAccess.Write, FileShare.None)) // Krijon nje file me emrin e dhene, emri ketu eshte dhene standart testing.gz
+                    using (GZipStream str = new GZipStream(outFile, CompressionMode.Compress)) // Krijon nje Zip Stream ku do te kompresohen filet
+                        CompressFile(sFilePath, str);
+
+                }
+            }
+        }
+
+        private void dekompreso_button_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                stopwatch = Stopwatch.StartNew();
+                Console.WriteLine(fbd.SelectedPath.ToString());
+                string path = fbd.SelectedPath.ToString();
+                Console.WriteLine("Pathi eshte: " + path);
+
+                DecompressToDirectoryV2(path, zipPath, checkBox1.Checked);
+                stopwatch.Stop();
+                label1.Text = "Koha eshte:" + stopwatch.ElapsedMilliseconds + "  ms";
+                Console.WriteLine("************ " + stopwatch.ElapsedMilliseconds);
+
+            }
+        }
+
+        bool DecompressFile(string path, GZipStream zipStream)
+        {
+
+            List<Byte> recivedBytes = new List<byte>();
+
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = zipStream.Read(buffer, 0, buffer.Length);
+                if (bytesRead <= 0)
+                {
+                    break;
+                }
+                byte[] correctBuffer = new byte[bytesRead];
+                Array.Copy(buffer, correctBuffer, bytesRead);
+
+                recivedBytes.AddRange(correctBuffer);
+            }
+
+
+            byte[] decompressedBytes = recivedBytes.ToArray();
+            string sFinalDir = Path.GetDirectoryName(path);
+            if (!Directory.Exists(sFinalDir))
+                Directory.CreateDirectory(sFinalDir);
+
+            using (FileStream outFile = new FileStream(path.Replace(".gz",""), FileMode.Create, FileAccess.Write, FileShare.None))
+                outFile.Write(decompressedBytes, 0, decompressedBytes.Length);
+
+            return true;
+        }
+
+        void DecompressToDirectoryV2(string sCompressedFileDir, string sDir, bool multi)
+        {
+            string[] sFiles = Directory.GetFiles(sCompressedFileDir, "*.gz*", SearchOption.AllDirectories); //Kthen te gjithe filet ne direktorin e dhene
+            foreach (string sFilePath in sFiles)
+            {
+                string sRelativePath = sFilePath.Substring(sCompressedFileDir.Length + 1); //File ka emrin e till C:\test\filename.txt kjo merr emrin relativ pra filename.txt
+               
+                if (multi)
+                {
+                    new Thread(() => {
+                        using (FileStream inFile = new FileStream(sFilePath, FileMode.Open, FileAccess.Read, FileShare.None)) // Hap file-in ne read mode
+                        using (GZipStream zipStream = new GZipStream(inFile, CompressionMode.Decompress, true))//Vendos File-in ne stream
+                            DecompressFile(sDir + "\\" + sRelativePath, zipStream); 
+
+                    }).Start();
+                }
+                else
+                {
+                    using (FileStream inFile = new FileStream(sFilePath, FileMode.Open, FileAccess.Read, FileShare.None)) // Hap file-in ne read mode
+                    using (GZipStream zipStream = new GZipStream(inFile, CompressionMode.Decompress, true))//Vendos File-in ne stream
+                        while (DecompressFile(sDir + "\\" + sRelativePath, zipStream)) ;
+
+                }
+            }
+
+        }
+    }
+
+}
